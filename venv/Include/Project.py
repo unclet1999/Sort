4b7da6252d8ddeb295f30a8d919682bdbae7726a
unclet1999 <real.friend2013@outlook.com>
2019-10-24 17:40:47 +0330

    Selection Sort and Merge Sort Added

--- venv/Include/Project.py
+++ venv/Include/Project.py
@@ -35,6 +35,42 @@ def bubble_sort(elements):
     write_to_file(elements,"Bubble_Sort")
 
 
+def selection_sort(elements):
+    for i in range(0, len(elements)):
+        min_index = i
+        for j in range(i + 1, len(elements)):
+            if elements[min_index] > elements[j]:
+                min_index = j
+
+        elements[i], elements[min_index] = elements[min_index], elements[i]
+    write_to_file(elements, "Selection_Sort")
+
+def merge_sort(elements):
+    if(len(elements)>1):
+        mid = len(elements)//2
+        L=elements[:mid]
+        R=elements[mid:]
+        merge_sort(L)
+        merge_sort(R)
+        i,j,k=0,0,0
+        while(i<len(L) and j<len(R)):
+            if L[i]<R[j]:
+                elements[k]=L[i]
+                i+=1
+            else:
+                elements[k]=R[j]
+                j+=1
+            k+=1
+        while(i<len(L)):
+            elements[k] = L[i]
+            i+=1
+            k+=1
+        while(j<len(R)):
+            elements[k]=R[j]
+            j+=1
+            k+=1
+
+
 def iterate_and_bubble_sort():
     global init_time
     for elements in read_from_file("Random.txt"):
@@ -45,12 +81,35 @@ def iterate_and_bubble_sort():
         f.write("Algorithm <Bubble_Sort> took "+str(duration)+" seconds to finish\n")
 
 
+def iterate_and_merge_sort():
+    global init_time
+    for elements in read_from_file("Random.txt"):
+        merge_sort(elements)
+        write_to_file(elements,"Merge_Sort")
+    with open("duration.txt",mode="a") as f:
+        final_time = time.perf_counter()
+        duration = final_time-init_time
+        f.write("Algorithm <Merge_Sort> took "+str(duration)+" seconds to finish\n")
+
+def iterate_and_selection_sort():
+    global init_time
+    for elements in read_from_file("Random.txt"):
+        selection_sort(elements)
+    with open("duration.txt",mode="a") as f:
+        final_time=time.perf_counter()
+        duration=final_time-init_time
+        f.write("Algorithm <Selection_Sort> took "+str(duration)+" seconds to finish\n")
+
+
 def main():
     global init_time
     init_time=time.perf_counter()
     t1=Thread(target=iterate_and_bubble_sort)
     t1.start()
-
+    t2 = Thread(target=iterate_and_selection_sort)
+    t2.start()
+    t3 = Thread(target=iterate_and_merge_sort)
+    t3.start()
 
 if __name__ == '__main__':
     main()
